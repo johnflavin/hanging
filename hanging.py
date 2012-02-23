a2dab3f8495ca082801ad4470f5b20c2771eeccb
Flavin <flavinj@gmail.com>
2012-02-23 14:57:30 -0600

    python 3.x compatible. removed filter lines in favor of lists

--- hanging.py
+++ hanging.py
@@ -1,4 +1,5 @@
 import sys
+import csv
 from string import ascii_uppercase as upperc
 
 target = sys.argv[1]
@@ -29,52 +30,55 @@ if '.' in target:
 	if wordlen>8:
 		sys.exit('Target word too long')
 
-	letterpos = [(i,target[i]) for i in range(len(target)) if target[i] is not '.']
+	letters = [char for char in target if char != '.']
+	indices = [target.index(char) for char in letters]
+	num_letters = len(letters)
 	try:
-		last_vowel_pos = max([letterpos[i][0] for i in range(len(letterpos)) if \
-				letterpos[i][1] in 'AEIOU'])
+		last_vowel_pos = max([indices[i] for i in range(num_letters) if letters[i] in 'AEIOU'])
 	except ValueError:
 		last_vowel_pos = -1
 
 	#Read in the string of words from the dictionary file
-	dictfile = 'dict'+str(wordlen)+'.txt'
-	with open(dictfile,'r') as f:
-		fulldictstring = f.read()
+	dictfile = 'dict{0}.csv'.format(wordlen)
+	try:
+		f = open(dictfile,'r')
+	except:
+		sys.exit('could not open dictionary file')
+	reader = csv.reader(f)
 
 	#List of all words in the dictionary that are the same length as target
-	fulldict = [fulldictstring[ (wordlen+1)*i : (wordlen+1)*i+wordlen ] \
-			for i in range(int(len(fulldictstring)/(wordlen+1))+1)]					# PYTHON 3: TypeError: 'float' object cannot be interpreted as an integer
+	fulldict = []
+	for line in reader:
+		fulldict += line
+	f.close()
+
 	#Keep only those words with the target letters in proper spaces
-	filterdict = list(filter(lambda word: all( [letterpos[i][1]==word[letterpos[i][0]] \
-				for i in range(len(letterpos)) ]),fulldict))
+	filterdict = [word for word in fulldict if all( [letters[i]==word[indices[i]] for i in range(num_letters) ] )]
+
 	#Keep only those words with none of the already guessed wrong letters
-	filterdict = list(filter(lambda word: all([l not in word for l in wronglets]), \
-				filterdict))
+	filterdict = [word for word in filterdict if all([lett not in word for lett in wronglets])]
+
 	#Keep only those words with correct letters ONLY in correct spaces, nowhere else
-	filterdict = list(filter(lambda word: all([target.count(l)==word.count(l) for l in \
-				set(zip(*letterpos)[1]) ]),filterdict))
+	filterdict = [word for word in filterdict if all([target.count(lett)==word.count(lett) for lett in set(letters)])]
+
 	#Keep only words with no vowels beyond last known vowel (which is always given)
-	filterdict = list(filter(lambda word: \
-				all([word[i] not in 'AEIOU' for i in range(last_vowel_pos+1,len(word))]),\
-				filterdict))
+	filterdict = [word for word in filterdict if all([lett not in 'AEIOU' for lett in word[last_vowel_pos+1:] ])]
+
 
 	# filterdict now holds only valid words. We must find the most common letter.
-	# We start by creating an array to hold a count of how many words contain 
-	# each letter.
+	# We create an array to hold a count of how many words contain each letter.
 	if len(filterdict) == 0:
 		sys.exit("You've eliminated all valid words!")
 	lettersbyword = [0]*26
 	for word in filterdict:
-		nontargetletters = list(filter(lambda l: l not in zip(*letterpos)[1], \
-						set(word)))
-		for letter in nontargetletters:
-			lettersbyword[upperc.index(letter)]+=1
+		for letter in set(word):
+			if letter not in letters:
+				lettersbyword[upperc.index(letter)]+=1
 
 	# The max value of this array is the frequency of most common letter.
 	maxletternum = max(lettersbyword)
-	# There might be more than one letter in this number of words.
-	maxletterlist = [upperc[i] for i in range(len(lettersbyword)) if \
-				lettersbyword[i] == maxletternum]
+	# There might be more than one letter with this frequency, so list them all.
+	maxletterlist = [upperc[i] for i in range(26) if lettersbyword[i] == maxletternum]
 	
 	if len(maxletterlist)>1:
 		print('Letters in most words: '+' '.join(maxletterlist))
@@ -91,35 +95,35 @@ else:
 		sys.exit("Put in 12 letters to get all words.")
 	
 	raw_wants = sys.argv[2:]
-	wants = [l.upper() for l in set(raw_wants)]
+	wants = [lett.upper() for lett in set(raw_wants)]
 
 	for i in range(5):
 		x = 8-i
-		dictfile = 'dict'+str(x)+'.txt'
-		with open(dictfile,'r') as f:
-	                fulldictstr = f.read()
-		
-		#Full dictionary of words of length x
-		fulldict = [fulldictstr[(x+1)*j:(x+1)*j+x] \
-				for j in range(int(len(fulldictstr)/(x+1))+1)]
+
+		#Read in the string of words from the dictionary file
+		dictfile = 'dict{0}.csv'.format(x)
+		try:
+			f = open(dictfile,'r')
+		except:
+			sys.exit('could not open dictionary file')
+		reader = csv.reader(f)
+
+		#List of all words in the dictionary that are the same length as target
+		fulldict = []
+		for line in reader:
+			fulldict += line
+		f.close()
 
 		#Filter out words that have any non-target letters
-		filterdict = list(filter(lambda word: \
-						all( [word.count(l)<=target.count(l)\
-						for l in set(word)] ), \
-						fulldict))
+		filterdict = [word for word in fulldict if all( [word.count(lett)<=target.count(lett) for lett in set(word)] )]
 
 		#Filter out words that don't have all the letters we want
-		if len(wants)>0:
-			filterdict = list(filter(lambda word: \
-								all( [l in word for l in wants]),\
-								filterdict))
+		filterdict = [word for word in filterdict if all( [lett in word for lett in wants] )]
 
 		# Printing the words
 		print('{0} letter words:'.format(x))
 
-		num_words = len(filterdict)
-		if num_words == 0:
+		if len(filterdict) == 0:
 			print('----')
 		else:
 			print_words(filterdict,x)
